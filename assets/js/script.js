4f2f20758b0dedd662437506f5fd73cc2861295e
Rene Ortega <reneortega2468@gmail.com>
2022-04-07 17:16:10 -0400

    added html, css

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const searchHistory =[]; 
+const weatherApiUrl = ""
+const apiKey = ""; 
+
+
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+
+
+
+
+
+
+
+
+
+function weatherDay (weatherApiUrl) {
+
+
+}
