40fe16cd89752ca8ec19d8eb4738d95608653a16
Rene Ortega <reneortega2468@gmail.com>
2022-04-07 18:08:29 -0400

    html, css DONE!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 const searchHistory =[]; 
-const weatherApiUrl = ""
-const apiKey = ""; 
+const weatherApiUrl = "https://api.openweathermap.org/"; 
+const apiKey = "c742f6a434aba29ee3de171e2674ca24"; 
 
 
-
+console.log("ayo")
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
+let searchForm 
+let searchInput
+let todayBox
+let forecastBox
+let searchDisplayBox
 
 
 
 
 
 
-
-
+var weatherApi = ``; 
 
 function weatherDay (weatherApiUrl) {
-
-
+  fetch(weatherApi)
+  .then(function (response) {
+    return response.json();
+  })
+  console.log(data)
 }
