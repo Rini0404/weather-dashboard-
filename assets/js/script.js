a810f5779b82bf475ad9d88fbc7c15a3ba08768e
Rene Ortega <reneortega2468@gmail.com>
2022-04-08 19:48:26 -0400

    js....

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
 // const = $('#') if i need to declare tings ye 
 // important stuffs ye 
 // cTemp = CURRENT shorthand tings ye
-
-const apiKey = "c742f6a434aba29ee3de171e2674ca24"; 
 const inputL = $('#citySearch');
 const cTemp = $('#temp')
 const cWind= $('#wind')
 const cHumid= $('#humidity')
 const cUv= $('#uv')
 const historyL = $('#historyEl')
-let searchHistory = JSON.parse(localStorage.getItem('search')) || []; 
+let sHistory = JSON.parse(localStorage.getItem('search')) || []; 
 const clearBtn = $('#clear')
-console.log(searchHistory);
 let searchForm  = $('#searchBtn');
 const cCity = $('#cityName')
 // cCity is added to the url, so when typed out, we grab the name an add it to city
-var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cCity}&appid=${apiKey}`
-
+var weatherApiRoot = `http://api.openweathermap.org`; 
+// var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cCity}&appid=${apiKey}`;
+const apiKey = "c742f6a434aba29ee3de171e2674ca24"; 
 // lets grab the users input! aka, searched city 
 function getWeather (cCity) {
   
@@ -25,6 +23,7 @@ function getWeather (cCity) {
     method: "GET", 
   })
   .then(function (response) {
+    // more vars for  
     console.log(response);
     const currentDate = new Date(response.data.dt*1000) ; 
     console.log(currentDate)
@@ -36,14 +35,73 @@ function getWeather (cCity) {
 
   })
 
+  
+// if conditionals 
+
+}
+
+function renderSearch (){
+  // ().innerHTML
+  // for loop
+}
+
+// update localStorage function need json.Stringify search history
+
+searchBtn.addEventListener("click",function() {
+  const searchedCity = inputL.value;
+  getWeather(searchedCity);
+  sHistory.push(searchedCity);
+  localStorage.setItem("search",JSON.stringify(sHistory));
+  rendersHistory();
+})
+
+
+
+
+
+renHistory();
+if (sHistory.length > 0) {
+    getWeather(sHistory[sHistory.length - 1]);
+}
+
+
+function renHistory() {
+  historyL.innerHTML = "";
+  for (let i = 0; i< sHistory.length; i++) {
+      const historyI = document.createElement("input");
+      historyI.setAttribute("type","text");
+      historyI.setAttribute("readonly",true);
+      historyI.setAttribute("class", "form-control d-block bg-white");
+      historyI.setAttribute("value", sHistory[i]);
+      historyI.addEventListener("click",function() {
+          getWeather(historyI.value);
+      })
+      historyL.append(historyI);
+  }
+  
+  renHistory(); 
+    if (sHistory.length > 0)
+      getWeather(sHistory[sHistory.length - 1]); 
 
 }
 
 
 
+// clearEl.addEventListener("click",function() {
+//   searchHistory = [];
+//   renderSearchHistory();
+// })
+
+// pull history from localStorage function  parse the .json
 
 
 
+// function for 5 day for loops 
+
+
+// for moment js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
 
 
 
@@ -58,8 +116,6 @@ function getWeather (cCity) {
 //   console.log(weatherUrl); 
 // }
 // console.log("ayo")
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
 
 // console.log(searchForm);
 // let searchInput
